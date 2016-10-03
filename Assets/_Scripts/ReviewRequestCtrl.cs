a4ab2996282181b08e0c0333a583d472caa93416
toshiro <tsubasa.oshiro@gmail.com>
2016-10-03 22:16:23 +0900

    レビュー実装修正。

--- Assets/_Scripts/ReviewRequestCtrl.cs
+++ Assets/_Scripts/ReviewRequestCtrl.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class ReviewRequestCtrl : MonoBehaviour {
 	ResultCtrl _resultCtrl;
 
+	void Start () {
+		_resultCtrl = this.gameObject.GetComponent<ResultCtrl> ();
+	}
+
 	void ReviewRequest () {
 		// プレイ回数がx回以上のユーザーに対して
 		int playCount = _resultCtrl._gameCtrl._userData.playCount;
 		if (playCount <= Const.INTERVAL_REVIEW_REQUEST || // 規定回以上プレイていない
-		    reviewDoneFlg) // レビュー済み
+		    _resultCtrl._gameCtrl._userData.reviewDoneFlg == 1) // レビュー済み
 		{
 			return; // 何もしない
 		}
@@ -26,8 +30,8 @@ public class ReviewRequestCtrl : MonoBehaviour {
 						//「ありがとうございます！よければレビューお願いします」
 						AskForReview ();
 					} else {
-						// メッセージ送信なら
-						if (!messageDoneFlg) {
+						// メッセージ送信したことないなら
+						if (_resultCtrl._gameCtrl._userData.messageDoneFlg == 1) {
 							// Webに飛ばす
 							AskForMessage ();
 						}
@@ -37,7 +41,6 @@ public class ReviewRequestCtrl : MonoBehaviour {
 		}
 	}
 
-	bool dontAskFlg;
 	bool reviewDoneFlg;
 	bool messageDoneFlg;
 	void AskForReview () {
@@ -49,7 +52,9 @@ public class ReviewRequestCtrl : MonoBehaviour {
 			"Would you review our app?  We'll be so happy to hear from you!",
 			(bool result) => {
 				if (result) {
-					setReviewDoneFlg (true);
+					_resultCtrl._gameCtrl._userData.reviewDoneFlg = 1;
+					_resultCtrl._gameCtrl._userData.save ();
+
 					// ストアへ
 					Application.OpenURL (Const.APP_STORE_URL);
 				}
@@ -66,17 +71,14 @@ public class ReviewRequestCtrl : MonoBehaviour {
 			"We'd love to hear your comment, advice, bug report and so in order to make our app better.  If you don't mind:",
 			(bool result) => {
 				if (result) {
-					setMessageDoneFlg (true);
+					_resultCtrl._gameCtrl._userData.messageDoneFlg = 1;
+					_resultCtrl._gameCtrl._userData.save ();
+
 					// Webビュー
 					Application.OpenURL (Const.SUPPORT_URL);
 				}
 			}
 		);	
-	}
-
-	void setReviewDoneFlg (bool pFlg) {
-		reviewDoneFlg = pFlg;
-
 	}
 
 	void setMessageDoneFlg (bool pFlg) {
