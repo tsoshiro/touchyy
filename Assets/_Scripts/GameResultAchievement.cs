d84790b4996a8d57e764bcd2cb93c06d1eb59be0
toshiro <tsubasa.oshiro@gmail.com>
2016-10-12 00:58:28 +0900

    Android版申請準備。

---
+++ Assets/_Scripts/GameResultAchievement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameResultAchievement {
+	public string achievementId;
+	public int targetCount;
+
+	public GameResultAchievement (string pAchievementId, int pTargetCount) {
+		achievementId = pAchievementId;
+		targetCount = pTargetCount;
+	}
+
+	public float getProgress (int pScore) {
+		float progress = (float)pScore / (float)targetCount;
+		return progress;
+	}
+}
